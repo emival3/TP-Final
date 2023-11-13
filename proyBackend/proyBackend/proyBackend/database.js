79458a0cf05ed44ebbc7050a77479163f1aa3a9a
emival3 <emivaleriano03@gmail.com>
2023-11-13 01:31:01 -0300

    first commit

---
+++ proyBackend/proyBackend/proyBackend/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+//const URI ='mongodb+srv://CentroSaludAdmin:AkvBeyooWtrPoJer@puestodesalud.jxrrtnf.mongodb.net/';
+
+const URI ='mongodb+srv://centrosaludjujuy:HqyMnE4nK4NYcaLy@puestodesalud.sqakm7r.mongodb.net/';
+
+
+//const URI= 'mongodb+srv://emival3:<password>@puestodesalud.jxrrtnf.mongodb.net/'
+//const URI = 'mongodb://127.0.0.1:27017/pysw-grupo8-tpfinal';
+//const URI = 'mongodb://agustina:admin@127.0.0.1:27017/DBCENTRODESALUD';
+
+mongoose.connect(URI)
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err))
+module.exports = mongoose;
---
+++ proyFrontend/proyFrontend/src/app/services/usuario.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  hostBase: string;
+
+  constructor(private http:HttpClient) {
+    this.hostBase = "http://3.82.255.160:3000/api/usuario/";
+   }
+
+  //todos los pacientes
+  getUsuarios():Observable<any>{
+    let httpOptions={
+      headers: new HttpHeaders(
+        {
+
+        }
+      ),
+      params: new HttpParams()
+
+    }
+
+    return this.http.get(this.hostBase,httpOptions);
+
+  }
+  //paciente por dni
+  getUsuarioDni(dni:string){
+    let httpOptions={
+      headers: new HttpHeaders(
+        {
+
+        }
+      ),
+      params: new HttpParams()
+      .append("dniU",dni)
+    }
+    return this.http.get(this.hostBase+"dni",httpOptions);
+  }
+
+  deleteUsuario(id:string):Observable<any>{
+    let httpOptions={
+      headers: new HttpHeaders(
+        {
+
+        }
+      ),
+      params: new HttpParams()
+    }
+
+    return this.http.delete(this.hostBase+id,httpOptions);
+  }
+
+  editUsuario(usuario:Usuario):Observable<any>{
+    let httpOptions={
+      headers: new HttpHeaders(
+        {
+          "Content-type": "application/json"
+        }
+      ),
+      params: new HttpParams()
+    }
+
+    let body = JSON.stringify(usuario);
+
+    return this.http.put(this.hostBase+usuario._id,body,httpOptions);
+  }
+}
